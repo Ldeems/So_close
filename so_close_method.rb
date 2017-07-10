27d790bf80cea4910e5cf9c1ba62440fbf3af9c0
Ldeems <luke.deems@yahoo.com>
2017-07-10 15:34:20 -0400

    testing win and no win

--- so_close_method.rb
+++ so_close_method.rb
@@ -2,6 +2,17 @@
 
 
 
-def grandbash
-	g = []
-end	
+def grandbash(ticket, winners)
+	matches = []
+	winners.each do |winners|
+		if winners == ticket
+		matches << ticket
+		end
+	end		
+	matches
+end
+
+
+#your number is 5678
+# your_ticket = "5678"
+# winning_numbers = ["5678", "3567", "0987"]
--- so_close_test.rb
+++ so_close_test.rb
@@ -8,6 +8,27 @@ class So_close_test < Minitest::Test
 	end	
 
 	def test_empty_array
-		assert_equal([],grandbash)
+	 	ticket = "1234"
+	 	winners = []
+	 	assert_equal([],grandbash(ticket, winners))
+	 end
+
+	def test_no_matches
+		ticket = "1234"
+		winners = ["5678", "3567", "0987"]
+		assert_equal([], grandbash(ticket, winners))
 	end	
+
+	def test_your_winning_number
+	 	ticket = "1234"
+	 	winners = ["1234", "3567", "0987"]
+	 	assert_equal(["1234"], grandbash(ticket, winners))
+	end
+
+	# def test_how_far_off
+	# 	ticket = "1234"
+	# 	winners = ["5064", "1235", "4532"]
+		
+	# end	
+
 end		
