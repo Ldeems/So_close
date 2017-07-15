4ae987b716f1e2a421b57f1bcfddc1ca6b2aad51
Ldeems <luke.deems@yahoo.com>
2017-07-15 16:28:24 -0400

    forgot to commit for a while, even changed a couple codes lol, but got it working for multiple size number, gonna do some more test still

--- so_close_method.rb
+++ so_close_method.rb
@@ -1,18 +1,64 @@
+def grandbash(ticket,winning_ticket)
+    matches = []
+    
+    winning_ticket.each do |winner|
+        if  winner == ticket
+            matches << ticket
+        end
+    end    
 
-
-
-
-def grandbash(ticket, winners)
-	matches = []
-	winners.each do |winners|
-		if winners == ticket
-		matches << ticket
-		end
-	end		
-	matches
+    unless matches.any?
+        one_off?(ticket,winning_ticket)
+    else
+        matches
+    end
 end
 
 
-#your number is 5678
-# your_ticket = "5678"
-# winning_numbers = ["5678", "3567", "0987"]
+def one_off?(ticket,winner)
+    
+    one_off_arr = []
+
+    unless ticket.empty?
+        
+        ticket_arr = ticket.chars
+
+            
+        winner.each do |win|
+            one_off_counter = 0
+            
+            winner_arr = win.chars
+
+            index_counter = 0
+
+                winner_arr.each do |num|
+                
+                    unless num == ticket_arr[index_counter]
+                    one_off_counter += 1
+                    end
+
+                    index_counter += 1
+                end
+            
+            if one_off_counter == 1
+            one_off_arr << win
+            end
+        end
+    end    
+
+one_off_arr
+end
+
+
+
+
+
+
+
+
+
+
+			
+
+
+
--- so_close_test.rb
+++ so_close_test.rb
@@ -3,32 +3,70 @@ require_relative "so_close_method.rb"
 
 class So_close_test < Minitest::Test
 
-	def test_1_equals_1
-		assert_equal(1,1)
-	end	
+ def test_1_equals_1
+        assert_equal(1,1)
+    end
 
-	def test_empty_array
-	 	ticket = "1234"
-	 	winners = []
-	 	assert_equal([],grandbash(ticket, winners))
-	 end
+    def test_for_empty_array
+        ticket = []
+        winner = []
+        assert_equal([],grandbash(ticket,winner))
+    end
 
-	def test_no_matches
-		ticket = "1234"
-		winners = ["5678", "3567", "0987"]
-		assert_equal([], grandbash(ticket, winners))
-	end	
+    def test_returns_winner_array_empty
+        ticket = "1243"
+        winner = []
+        assert_equal([],grandbash(ticket,winner))
+    end
 
-	def test_your_winning_number
-	 	ticket = "1234"
-	 	winners = ["1234", "3567", "0987"]
-	 	assert_equal(["1234"], grandbash(ticket, winners))
-	end
+    def test_your_ticket_with_a_match
+     ticket = "1234"
+     winner = ["1234", "5555", "8789"]
+     assert_equal(["1234"], grandbash(ticket,winner))
+    end
 
-	# def test_how_far_off
-	# 	ticket = "1234"
-	# 	winners = ["5064", "1235", "4532"]
+    def test_ticket_no_match
+        ticket = "5432"
+        winner = ["1234", "5555", "8789"]
+        assert_equal([],grandbash(ticket,winner))
+    end
+
+    def test_one_off
+        ticket = "1234"
+        winner = ["1235","5555","8789"]
+        assert_equal(["1235"],one_off?(ticket,winner))
+    end
+
+    def test_one_off_return_multiple_numbers
+        ticket = "5432"
+        winner = ["5438", "5555", "5434"]
+        assert_equal(["5438","5434"],one_off?(ticket,winner))
+    end
+    
+    def test_gandbash_return_multiple_numbers
+        ticket = "5432"
+        winner = ["5438", "5555", "5434"]
+        assert_equal(["5438","5434"],grandbash(ticket,winner))
+    end
+
+    def test_more_digits_match
+    	ticket = "687524"
+    	winner = ["123456", "687524","456974"]
+    	assert_equal(["687524"], grandbash(ticket,winner))
+    end	
+
+
+    def test_more_digits_off_one
+    	ticket = "456789"
+    	winner = ["123456", "556789","456974"]
+    	assert_equal(["556789"], grandbash(ticket,winner))
+    end	
+end
+
+
+
+
+
+
+
 		
-	# end	
-
-end		
